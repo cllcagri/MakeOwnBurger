54c07204f87f2610f54faa4b38d2425a0c192204
celal <c.cagriyilmaz@gmail.com>
2020-05-17 01:56:36 +0300

    Creating Burger Ingredients Component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const Burger = (props) => {
+  return(
+      <div className="Burger">
+          <BurgerIngredient type="bread-top" />
+          <BurgerIngredient type="meat" />
+          <BurgerIngredient type="cheese" />
+          <BurgerIngredient type="bread-bottom" />
+      </div>
+  );
+};
+export default Burger;
